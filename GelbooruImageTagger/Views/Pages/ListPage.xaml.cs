3398f1a10ebe7146de26a5ef6c0b4312846dc9ab
nighteden <60023068+nightedenart@users.noreply.github.com>
2022-04-30 00:56:05 +0100

    Initial commit

---
+++ GelbooruImageTagger/Views/Pages/ListPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using GelbooruImageTagger.Models;
+using GelbooruImageTagger.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GelbooruImageTagger.Views.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ListPage : Page
+    {
+        public ListPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.DataContext is MainViewModel vm && sender is ListView listView)
+            {
+                vm.SelectedGelbooruImage = (GelbooruImage)listView.SelectedItem;
+
+                if (listView.SelectedItems.Count > 0)
+                {
+                    vm.AddRangeObservable(vm.SelectedGelbooruImages, listView.SelectedItems.Cast<GelbooruImage>().ToArray(), true);
+                }
+                else
+                {
+                    vm.SelectedGelbooruImages.Clear();
+                }
+
+                vm.RefreshSelection();
+            }
+        }
+
+        public static T FindVisualParent<T>(UIElement element) where T : UIElement
+        {
+            UIElement parent = element;
+            while (parent != null)
+            {
+                if (parent is T correctlyTyped)
+                {
+                    return correctlyTyped;
+                }
+
+                parent = VisualTreeHelper.GetParent(parent) as UIElement;
+            }
+
+            return null;
+        }
+
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame parentFrame = FindVisualParent<Frame>(SettingButton);
+
+            if (parentFrame != null)
+            {
+                parentFrame.Navigate(typeof(SettingPage));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# GelbooruImageTagger
+Universal Windows Platform (UWP) app that paints metadata properties to images downloaded from Gelbooru.
+
+Currently, this app paints metadata such as any artists, copyrights / intellectual properties, keywords / tags and image source.
+
+This makes images easier to search for a particular tag or author.
+
+## How to use
+
+Typically, images downloaded from Gelbooru are in the form of an MD5 hash (e.g. 5f97439d98b85503c6419c2aa7521a01.jpg) or prefixed with "sample_" if it's a thumbnail.
+
+Add the downloaded files to the list and click 'Tag' when ready.
+
+You can also go into the settings to skip images that have already been tagged.
+
+As this is a UWP app, you will need to install Visual Studio 2022 (recommended) and install "Universal Windows Platform development" tools to build and run. This app isn't currently on the Microsoft Store but is under consideration.
+
+## Notes
+
+This project and repository is not affiliated with nor endorsed by Gelbooru in any way. I made the project as a hobby and for personal use.
