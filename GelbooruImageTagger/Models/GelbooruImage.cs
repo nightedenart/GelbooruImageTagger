7aa7e1ca1093700463aef0e72b7212909f9a802a
nighteden <60023068+nightedenart@users.noreply.github.com>
2022-05-01 18:54:05 +0100

    Add GelbooruImage model

---
+++ GelbooruImageTagger/Models/GelbooruImage.cs
@@ -0,0 +1,52 @@
+﻿using GelbooruImageTagger.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GelbooruImageTagger.Models
+{
+    public class GelbooruImage : BindableBase
+    {
+        private string? _path;
+        private ImageSource? _thumbnail;
+        private ObservableCollection<string> _tags = new();
+        private ObservableCollection<string> _artists = new();
+        private ObservableCollection<string> _copyrights = new();
+        private string? _sourceUri;
+
+        public string? Path
+        {
+            get => _path;
+            set => SetField(ref _path, value);
+        }
+        public ImageSource? Thumbnail
+        {
+            get => _thumbnail;
+            set => SetField(ref _thumbnail, value);
+        }
+        public ObservableCollection<string> Tags
+        {
+            get => _tags;
+            set => SetField(ref _tags, value);
+        }
+        public ObservableCollection<string> Artists
+        {
+            get => _artists;
+            set => SetField(ref _artists, value);
+        }
+        public ObservableCollection<string> Copyrights
+        {
+            get => _copyrights;
+            set => SetField(ref _copyrights, value);
+        }
+        public string? SourceUri
+        {
+            get => _sourceUri;
+            set => SetField(ref _sourceUri, value);
+        }
+    }
+}
