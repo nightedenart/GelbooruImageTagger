9b6f9820f4a082d2cb66d1d9e3125280f24aaa98
nighteden <60023068+nightedenart@users.noreply.github.com>
2022-05-01 18:56:15 +0100

    Set up view model

---
+++ GelbooruImageTagger/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GelbooruImageTagger.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GelbooruImageTagger.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        #region Internal Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Commands
+        #endregion
+
+        #region Helper Methods
+        #endregion
+
+        #region Methods
+        #endregion
+
+    }
+}
