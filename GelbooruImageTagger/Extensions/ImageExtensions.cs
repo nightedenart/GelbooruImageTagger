24373810d039f4a0d1c16d46a0dbcca33da88b48
nighteden <60023068+nightedenart@users.noreply.github.com>
2022-05-01 19:54:53 +0100

    Add image extensions
    I'll need to convert a Bitmap to an ImageSource for thumbnail support.
    
    Note: Bitmap is from System.Drawing.Common package which can be obtained from NuGet.

---
+++ GelbooruImageTagger/Extensions/ImageExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GelbooruImageTagger.Extensions
+{
+    internal static class ImageExtensions
+    {
+        public static ImageSource? BitmapToImageSource(this Image? image)
+        {
+            if (image == null)
+                return null;
+
+            try
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                System.IO.MemoryStream memoryStream = new System.IO.MemoryStream();
+                image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+                bitmap.StreamSource = memoryStream;
+                bitmap.EndInit();
+                return bitmap;
+            }
+            catch {
+                return null;
+            }
+        }
+
+    }
+}
